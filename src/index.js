a6548825e0b12864cf78252d133e4e71008ae881
Sahit Katta <sahit.mohan@gmail.com>
2023-12-17 17:47:08 -0600

    static crud operation in GraphQL added prisma client

--- src/index.js
+++ src/index.js
@@ -4,41 +4,58 @@ import {
   ApolloServerPluginLandingPageLocalDefault,
   ApolloServerPluginLandingPageProductionDefault,
 } from "@apollo/server/plugin/landingPage/default";
+import { readFileSync } from "fs";
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-
-`;
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
 const resolvers = {
   Query: {
     info: () => `This is API for Hackernews Clone`,
     feed: () => links,
+    link: (id) => links.find(l => l.id === id), 
   },
   Link: {
     id: (parent) => parent.id,
     description: (parent) => parent.description,
     url: (parent) => parent.url,
+  },
+  Mutation: {
+    post: (_parent, args) => {
+      let idCount = links.length
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (_parent, args) => {
+      const link = links.find(l => l.id === args.id)
+      if(args.description){
+        link.description = args.description
+      }
+      if (args.url) {
+        link.url = args.url
+      }
+      return link
+    },
+    deleteLink: (_parent, args) => {
+      const link = links.find(l => l.id === args.id)
+      links = links.filter(l => l.id !== args.id)
+      return link
+    }
   }
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: readFileSync("src/schema.graphql", "utf-8"),
   resolvers,
   plugins: [
     process.env.NODE_ENV === "production"
